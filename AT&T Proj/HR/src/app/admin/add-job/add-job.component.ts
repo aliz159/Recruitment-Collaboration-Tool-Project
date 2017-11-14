22d3024a8b5f2ca9ef1448a5da95eab9a8a70950
yaffa207 <yaffa2077@gmail.com>
2017-11-14 17:55:39 +0200

    newjob1

--- AT&T Proj/HR/src/app/admin/add-job/add-job.component.ts
+++ AT&T Proj/HR/src/app/admin/add-job/add-job.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Job } from "./Job.interface";
+import { JobService } from "../../services/JobService/job.service";
 
 @Component({
   selector: 'app-add-job',
@@ -8,78 +9,134 @@ import { Job } from "./Job.interface";
 })
 export class AddJobComponent implements OnInit {
 
-  constructor() { }
+  constructor(private jobService: JobService) { }
 
-   ngOnInit() {
+  ngOnInit() { 
+    this.jobService.GetSkillSet().subscribe(rsp => {
+     if (rsp.status == 200) {
+        this.Skills = rsp.json();
+        
+        console.log("Array Skiils From DB");
+        console.log(this.Skills);
+      }
+      else { console.log("server responded error : " + rsp); }
+    },
+      (err) => {
+        console.log("error : " + err);
+      });
+
+
+
+    this.jobService.GetReccruitersManager().subscribe(rsp => {
+      if (rsp.status == 200) {
+        //debugger;
+        this.Managers = rsp.json();
+        console.log("Array Manager From DB");
+        console.log(this.Managers);
+      }
+      else { console.log("server responded error : " + rsp); }
+    },
+      (err) => {
+        console.log("error : " + err);
+      });
   }
 
-  Title:string;
-  Position="Developer";
-  YearsExperience:number;
-  Description:string;
-  Skills:string;
-  ArraySkills:string[] = [];
-  ArrayManagers:string[] = [];
-  AddManager:boolean=false;
+  Title: string;
+  uniqueID:string;
+  position = "Developer";
+  YearsExperience: number;
+  Description: string;
+  Skills: string[];
+  Managers: string[];
+  Requirements: string;
+  ArraySkills: string[] = [];
+  ArrayManagers: any[] = [];
+  AddManager: boolean = false;
+  isActive: boolean = false;
+  jobObj:any;
+  skillObj:any[];
 
-  SaveDatilesJob(){
-    console.log(this.Title , this.Position,
-    this.YearsExperience , this.Description
-    ,this.ArraySkills,this.ArrayManagers);
+  SaveDatilesJob() {
+    let allSkills = this.ArraySkills.toString();
+    
+    this.jobService.addJob(this.ArrayManagers[0],this.uniqueID, this.Title,this.position,
+      this.Description,this.YearsExperience, this.Requirements, this.isActive).subscribe(rsp => {
+        //if (rsp.status == 200) { 
+          this.jobObj = rsp;
+          console.log("job Obj=>>>>>>>>>>>>");
+          console.log(this.jobObj);
+      
+            debugger;
+             this.jobService.addSkillsetForJob(this.jobObj.Id,allSkills).subscribe(rsp => {
+             //if (rsp.status == 200) { 
+               debugger; 
+                this.skillObj = rsp;
+                //window.alert("skill Added successfully=>>>>>>>>>>")
+                console.log("skill Added successfully=>>>>>>>>>>");
+                console.log(this.skillObj);
+                window.alert("the job has Successfully added")
+             // }
+            //  else {debugger;  console.log("server responded error : " + rsp); }
+            },
+             (err) => { debugger; console.log("error : " + err); });
+
+
+        //}
+       // else {debugger; console.log("server responded error : " + rsp);//}
+    },
+      (err) => { debugger; console.log("error : " + err); });
+
+    console.log(
+      this.ArrayManagers[0], this.Title,
+      this.Description, allSkills,
+      this.Requirements, this.isActive)
   }
 
 
 
 
-  RecruitingMangersList(){
+  RecruitingMangersList() {
     this.AddManager = true;
-    this.RecruitingManagers ;
+    //   this.RecruitingManagers ;
   }
 
 
 
 
-  ManagersArray(manager){
- if(manager.selected){
-      this.ArrayManagers.push(manager.display);
+  ManagersArray(manager) {
+    if (manager.selected) {
+      this.ArrayManagers.push(manager.Id, manager.Name);
     }
-    else{
-      var removeManager = this.ArrayManagers.indexOf(manager.display);
-      this.ArrayManagers.splice(removeManager,1);
+    else {
+      var removeManager = this.ArrayManagers.indexOf(manager.Id);
+      this.ArrayManagers.splice(removeManager, 2);
     }
     console.log(this.ArrayManagers)
   }
 
-  SkillsetArray(skill){
-    if(skill.selected){
-      this.ArraySkills.push(skill.display);
+  SkillsetArray(skill) {
+    if (skill.selected) {
+      this.ArraySkills.push(skill.Id);
     }
-    else{
-      var removeSkill = this.ArraySkills.indexOf(skill.display);
-      this.ArraySkills.splice(removeSkill,1);
+    else {
+      var removeSkill = this.ArraySkills.indexOf(skill.Id);
+      this.ArraySkills.splice(removeSkill, 1);
     }
     console.log(this.ArraySkills)
   }
 
 
 
- addSkill(){
+  addSkill() {
   }
-
-
-   RecruitingManagers = [
-    { value: 'Avi', display: 'Avi' },
-    { value: 'Liat', display: 'Liat' },
-    { value: 'Yaffa', display: 'Yaffa' },
-
-  ];
-
-
-   Skills1 = [
-    { value: 'HTML', display: 'HTML' },
-    { value: 'CSS', display: 'CSS' },
-    { value: 'Python', display: 'Python' },
-    { value: 'C', display: 'C' },
-    { value: 'JavaScript', display: 'JavaScript' },
-  ];
+  //  RecruitingManagers = [
+  //   { value: 'Avi', display: 'Avi' },
+  //   { value: 'Liat', display: 'Liat' },
+  //   { value: 'Yaffa', display: 'Yaffa' },// ];
+  //  Skills1 = [
+  //   { value: 'HTML', display: 'HTML' },
+  //   { value: 'CSS', display: 'CSS' },
+  //   { value: 'Python', display: 'Python' },
+  //   { value: 'C', display: 'C' },
+  //   { value: 'JavaScript', display: 'JavaScript' },// ];
 }
--- AT&T Proj/HR/src/app/services/JobService/job.service.ts
+++ AT&T Proj/HR/src/app/services/JobService/job.service.ts
@@ -7,10 +7,16 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class JobService {
   private url: string;
+  private urlSkillSet:string;
+  private urlReccruitersManager:string;
+  private urlJobSkills:string
   headers: any;
 
   constructor( @Inject(Http) private http: Http) {
-    this.url = "http://localhost:55187/api/Job";
+    this.url = "http://localhost:55187/api/Job"; //DB Job
+    this.urlSkillSet = "http://localhost:55187/api/Skillsets";//DB skills
+    this.urlReccruitersManager = "http://localhost:55187/api/User";//DB Managers
+    this.urlJobSkills = "http://localhost:55187/api/JobSkills";//DB skillsForJob
     this.headers = new Headers({ 'Accept': 'application/json' })
   }
 
@@ -18,24 +24,46 @@ export class JobService {
   Get() {
     return this.http.get(this.url);
   }
-
-  addJob(userId: number, name: string, description: string,
-    requirements: string, isActive: boolean) {
-
+  GetSkillSet() {
+    return this.http.get(this.urlSkillSet);
+  }
+  GetReccruitersManager(){
+    return this.http.get(this.urlReccruitersManager);
+  }  
+  addJob(userId: number,uniqueID:string, name: string,position:string, description: string,
+    YearsExperience:number, requirements: string, isActive: boolean) {
+    //debugger;
     let url = this.url;
-    let body = { UserId: userId, Name: name, Description: description, 
-    Requirements: requirements, IsActive: isActive};
+    let body = { UserId: userId,strUniqueID: uniqueID, Name: name,Position: position, Description: description,
+    YearOfExperience:YearsExperience,Requirements: requirements, IsActive: isActive};
+
+    return this.http.post(url, body, this.headers).map((res) => {
+      return res.json();
+    });
+  }
+  addSkillsetForJob(Id:number,skillset:string){
+     let url = this.urlJobSkills;
+     let body = { JobId: Id,SkillsetsId: skillset};
 
     return this.http.post(url, body, this.headers).map((res) => {
       return res.json();
     });
   }
 
-  editJob(Id: number, userId: number, name: string, description: string, 
+
+  
+  
+
+ 
+
+
+
+
+  editJob(Id: number, userId: number, name: string, description: string, skillset: string, 
     requirements: string, isActive: boolean) {
 
     let url = this.url + "/" + Id;
-    let body = { id: Id, UserId: userId, Name: name, Description: description, 
+    let body = { id: Id, UserId: userId, Name: name, Description: description, Skillset: skillset,
     Requirements: requirements, IsActive: isActive};
 
     return this.http.put(url, body, this.headers).map((res) => {
