e26ecbf83653f18160876df7308762883d1fd397
Joseph Mahari <juzefma@gmail.com>
2017-11-26 20:59:57 +0200

    Navbar

--- AT&T Proj/HR/src/app/app.component.ts
+++ AT&T Proj/HR/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Http } from '@angular/http';
 import { NavbarService } from "./services/navBarService/navbar.service";
 import { UserService } from "./services/UsersService/user.service";
-import { CookiesService } from "./services/CookiesService/cookies.service";
+
 
 @Component({
   selector: 'app-root',
@@ -15,37 +15,34 @@ export class AppComponent implements OnInit {
   Password: string;
   Error: string;
   userObj: any;
-  JisClicked = false;
-  AisClicked = false;
-  //IsLogged:true;
+  search = false;
+  HRIsLoggedIn = false;
+  //IsLogged=true;
   ngOnInit() {
     this.nav.hideMenu();
     this.nav.showLoginForm();
   }
 
   constructor(public http: Http, public nav: NavbarService, 
-    public userService: UserService, public router: Router,
-  public cookiesService: CookiesService) {}
+    public userService: UserService, public router: Router) {}
 
   Login() {
     this.userService.UserConfirmation(this.Email, this.Password).subscribe((rsp: any) => {
       this.userObj = rsp;
-
       if (this.userObj != "user name or password is invalid") {
+        this.nav.showLoginForm();
         if (this.userObj.UserType == "Admin") {
+          this.HRIsLoggedIn = true;
           this.routeToAdminFramework();
           this.nav.showMenu();
           this.nav.hideLoginForm();
         }
         else if (this.userObj.UserType == "Recruiter") {
-          this.routeToRecruiterFramework(this.userObj.Id);
+          this.routeToRecruiterFramework();
           this.nav.showMenu();
           this.nav.hideLoginForm();
         }
-        this.cookiesService.setCoockie("Role",this.userObj.UserType)
       }
-
-
     },
       (err) => {
         this.Error = "Email or password incorrect Please try again";
@@ -53,8 +50,8 @@ export class AppComponent implements OnInit {
       });
   }
 
-  routeToRecruiterFramework(id:number) {
-    this.router.navigate(['/MyApplicants', id]);
+  routeToRecruiterFramework() {
+    this.router.navigate(['/MyApplicants']);
   }
 
   routeToAdminFramework() {
